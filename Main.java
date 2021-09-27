319b269c9a52db4833eda4c59aabd51c13b88014
mhpaffik <91080667+mhpaffik@users.noreply.github.com>
2021-09-27 14:26:04 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+      /*  // -------1-------
+        double galon = 3.7854;
+        double mnozstvi = scr.nextDouble();
+
+        double vysledek = galon * mnozstvi;
+        System.out.print("Je to " + vysledek + " litru");
+        */
+
+       /* //-------2------
+        double vaha_zem = scr.nextDouble();
+        double vaha_mes = vaha_zem * 0.17;
+
+        System.out.println("Vase vaha na mesici bude " + vaha_mes + " kg");
+        */
+
+
+       /* //----------3-------------
+        double polomer = scr.nextInt();
+        double obsah = polomer * polomer * 3.14;
+        double delka = polomer * 2 * 3.14;
+
+        System.out.println("Obshah je " + obsah + " a delka je " + delka);
+       */
+        scr.close();
+
+
+    }
+}
